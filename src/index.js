fb027a9d0cd657b43ba56dd40f910c38af2e2087
Shashank Chikattimala <shashankcm90@gmail.com>
2021-10-14 15:24:03 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const React = (function () {
+  let _val;
+  function useState(initVal) {
+    const state = _val || initVal;
+    const setState = (newVal) => {
+      _val = newVal;
+    };
+    return [state, setState];
+  }
+  function render(Component) {
+    const C = Component();
+    C.render();
+    return C;
+  }
+  return { useState, render };
+})();
+
+function Component() {
+  const [count, setCount] = React.useState(1);
+  return {
+    render: () => console.log(count),
+    click: () => setCount(count + 1)
+  };
+}
+
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
+App.click();
