81e32e825e4d957216f1a6d09045afa1a14be4cc
Shashank Chikattimala <shashankcm90@gmail.com>
2021-10-14 15:57:33 -0500

    added useState and useEffect https://www.youtube.com/watch?v=KJP1E-Y-xyo

--- src/index.js
+++ src/index.js
@@ -1,35 +1,43 @@
 const React = (function () {
-  let _val;
+  let hooks = [];
+  let index = 0;
   function useState(initVal) {
-    const state = _val || initVal;
+    const state = hooks[index] || initVal;
+    const _index = index;
     const setState = (newVal) => {
-      _val = newVal;
+      hooks[_index] = newVal;
     };
+    index++;
     return [state, setState];
   }
+  function useEffect(cb, depsArray) {
+    const oldDeps = hooks[index];
+    let hasChanged = true;
+    if (oldDeps) {
+      hasChanged = depsArray.some((dep, i) => !Object.is(dep, oldDeps[i]));
+    }
+    if (hasChanged) cb();
+    hooks[index] = depsArray;
+    index++;
+  }
   function render(Component) {
+    index = 0;
     const C = Component();
     C.render();
     return C;
   }
-  return { useState, render };
+  return { useState, render, useEffect };
 })();
 
 function Component() {
   const [count, setCount] = React.useState(1);
+  const [text, setText] = React.useState("apple");
+  React.useEffect(() => {
+    console.log("Heyyyy useEffect called");
+  }, []);
   return {
-    render: () => console.log(count),
-    click: () => setCount(count + 1)
+    render: () => console.log({ count, text }),
+    click: () => setCount(count + 1),
+    type: (word) => setText(word)
   };
 }
-
-var App = React.render(Component);
-App.click();
-var App = React.render(Component);
-App.click();
-var App = React.render(Component);
-App.click();
-var App = React.render(Component);
-App.click();
-var App = React.render(Component);
-App.click();
